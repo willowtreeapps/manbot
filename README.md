5a7f9e1974b10b179bab0a8ebdf44182d22badd0
Ian Terrell <ian.terrell@willowtreeapps.com>
2015-10-06 15:27:10 -0400

    Start making manbot

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
+# manbot
 
-# mybot
+`manbot` spits out man pages!
 
-`mybot` is an working Slack bot written in Go. Fork it and use it to build
-your very own cool Slack bot!
-
-Check the [blog post](https://www.opsdash.com/blog/slack-bot-in-golang.html)
-for a description of mybot internals.
-
-Follow us on Twitter today! [@therapidloop](https://twitter.com/therapidloop)
+`manbot` is a fork of `mybot` from [@therapidloop](https://twitter.com/therapidloop)'s
+[blog post](https://www.opsdash.com/blog/slack-bot-in-golang.html).
--- manbot.go
+++ manbot.go
@@ -1,6 +1,6 @@
 /*
 
-mybot - Illustrative Slack bot in Go
+manbot - Illustrative Slack bot in Go
 
 Copyright (c) 2015 RapidLoop
 
@@ -26,66 +26,53 @@ THE SOFTWARE.
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
+	"os/exec"
 	"strings"
+)
+
+const (
+	command   = "!man "
+	man       = "/usr/bin/man"
+	maxLength = 100
 )
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "usage: mybot slack-bot-token\n")
+		fmt.Fprintf(os.Stderr, "usage: manbot slack-bot-token\n")
 		os.Exit(1)
 	}
 
 	// start a websocket-based Real Time API session
-	ws, id := slackConnect(os.Args[1])
-	fmt.Println("mybot ready, ^C exits")
+	ws, _ := slackConnect(os.Args[1])
+	log.Println("manbot ready, ^C exits")
 
 	for {
 		// read each incoming message
 		m, err := getMessage(ws)
 		if err != nil {
+			log.Println("Exiting on error from getMessage")
 			log.Fatal(err)
 		}
 
 		// see if we're mentioned
-		if m.Type == "message" && strings.HasPrefix(m.Text, "<@"+id+">") {
-			// if so try to parse if
-			parts := strings.Fields(m.Text)
-			if len(parts) == 3 && parts[1] == "stock" {
-				// looks good, get the quote and reply with the result
-				go func(m Message) {
-					m.Text = getQuote(parts[2])
-					postMessage(ws, m)
-				}(m)
-				// NOTE: the Message object is copied, this is intentional
+		if m.Type == "message" && strings.HasPrefix(m.Text, command) {
+			log.Printf("Received request: '%v'", m.Text)
+			query := strings.TrimPrefix(m.Text, command)
+			args := strings.Split(query, " ")
+
+			out, err := exec.Command(man, args...).Output()
+			if err != nil {
+				m.Text = fmt.Sprintf("Error: %v", err)
 			} else {
-				// huh?
-				m.Text = fmt.Sprintf("sorry, that does not compute\n")
-				postMessage(ws, m)
+				if len(out) > maxLength {
+					out = append(out[0:maxLength], []byte("...")...)
+				}
+				m.Text = string(out)
 			}
+			postMessage(ws, m)
 		}
 	}
-}
-
-// Get the quote via Yahoo. You should replace this method to something
-// relevant to your team!
-func getQuote(sym string) string {
-	sym = strings.ToUpper(sym)
-	url := fmt.Sprintf("http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=nsl1op&e=.csv", sym)
-	resp, err := http.Get(url)
-	if err != nil {
-		return fmt.Sprintf("error: %v", err)
-	}
-	rows, err := csv.NewReader(resp.Body).ReadAll()
-	if err != nil {
-		return fmt.Sprintf("error: %v", err)
-	}
-	if len(rows) >= 1 && len(rows[0]) == 5 {
-		return fmt.Sprintf("%s (%s) is trading at $%s", rows[0][0], rows[0][1], rows[0][2])
-	}
-	return fmt.Sprintf("unknown response format (symbol was \"%s\")", sym)
 }
